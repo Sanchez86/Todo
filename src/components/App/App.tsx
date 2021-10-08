0c9e6f07ceb815737f19c507b64e533908faaf7b
Alexandr Konev <0719118@gmail.com>
2021-10-08 18:16:19 +0300

    Add ESLint (#2)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 function App() {
-
   return (
-    <div className="App">
-
-    </div>
+    <div className="App" />
   );
 }
 
