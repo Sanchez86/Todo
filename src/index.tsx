cbe81e1beccfd49bef3c30e524679202171472c8
Sanchez86 <0719118@gmail.com>
2021-10-08 16:23:25 +0300

    Add ESLint

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
+
 import store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 reportWebVitals();
