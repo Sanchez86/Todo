dab8a71beb62f588e762212f4d6397ac36b312f4
Alexandr Konev <0719118@gmail.com>
2021-10-18 13:53:36 +0300

    Feature/remove todo (#9)
    * Remove elemetn
    
    * Fix functional in reducer for ADD_ITEM
    
    * Fix remove element
    
    * Fix remove item
    
    * Fix remove item

--- src/store/reducers/index.jsx
+++ src/store/reducers/index.jsx
@@ -6,16 +6,20 @@ import {
 const lsData = localStorage.getItem('data');
 
 const initialState = {
-  data: lsData ? JSON.parse(lsData).data : [],
+  data: lsData ? JSON.parse(lsData) : [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case REMOVE_ITEM:
     {
+      const newData = state.data ? state.data.filter((item) => item.id !== action.payload) : [];
+
+      localStorage.setItem('data', JSON.stringify(newData));
+
       return {
         ...state,
-        data: state.data.filter((item) => item.id !== action.payload),
+        data: newData,
       };
     }
     case ADD_ITEM:
@@ -25,7 +29,7 @@ const reducer = (state = initialState, action) => {
         data: [...state.data, action.payload],
       };
 
-      localStorage.setItem('data', JSON.stringify(newData));
+      localStorage.setItem('data', JSON.stringify(newData.data));
 
       return newData;
     }
